49f4118db7baefabd676002e9af4e24941e68019
RishalAggarwal <rishal.aggarwal@gmail.com>
2018-03-26 05:25:37 +0530

    Update linked list.py

--- face detec + linked list/linked list.py
+++ face detec + linked list/linked list.py
@@ -9,8 +9,8 @@ class Linked_List:
 	def __init__(self):
 		self.head = node()
 
-	def create_cycle(self, data, data_of_next):			#method to create the loop, data_of_next is required to know which node to loop back to
-		new_node = node(data)
+	def create_cycle(self, data, data_of_next):			#method to create the loop, data_of_next is required to know 
+		new_node = node(data)					#which node to loop back to
 		cur = self.head
 		while cur.next != None:
 			cur = cur.next
@@ -38,11 +38,12 @@ class Linked_List:
 			cur1=cur1.next
 
 		if cur.next == None:
-			return node(-1)					#assuming all valid nodes are give positive integers for their data, this can be used to check if the linked list has a loop as -1 is passed as the data of the node which is returned by the function
-		else:
-			return cur
-
-	def display_cycle(self):				#displaying the cycle
+			return node(-1)					#assuming all valid nodes are give positive integers for their 
+		else:							#data, this can be used to check if the linked list has a loop 
+			return cur					#as -1 is passed as the data of the node which is returned by 
+									#the function
+									
+	def display_cycle(self):					#displaying the cycle
 
 		cur=Linked_List.locate_cycle(self)
 		if cur.data==-1:
@@ -111,8 +112,8 @@ class Linked_List:
 				last_node.next = cur_node.next
 				break
 
-	def insert(self, data, data_of_prev):			#inserting node at a certain location. Data of prev is the data of the node after whic a certain node has to be inserted
-		cur_node=self.head
+	def insert(self, data, data_of_prev):			#inserting node at a certain location. Data of prev is the data of the 
+		cur_node=self.head				#node after whic a certain node has to be inserted
 		while True:
 			cur_node=cur_node.next
 			if cur_node.data == data_of_prev:
@@ -127,14 +128,14 @@ my_list = Linked_List()
 my_list.append(1)
 my_list.append(2)
 my_list.append(3)
-my_list.append(4)
+my_list.append(4)						#[1,2,3,4]
 my_list.display()
-my_list.insert(7,3)
+my_list.insert(7,3)						#[1,2,3,7,4]
 my_list.display()
-my_list.remove(3)
+my_list.remove(3)						#[1,2,7,4]
 my_list.display()
-my_list.create_cycle(5,2)
-my_list.display()
-my_list.display_cycle()
-print my_list.length()
-print my_list.loop_length()
+my_list.create_cycle(5,2)					#[1,2,7,4,5,2]
+my_list.display()						#this will display [1,2,7,4,5]
+my_list.display_cycle()						#this will display [2,7,4,5,2]
+print my_list.length()						#5
+print my_list.loop_length()					#4
